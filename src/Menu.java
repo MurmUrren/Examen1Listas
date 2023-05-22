47e37982aba62fcf6da6aacfc2deabaa36d741b9
Luis Felipe <108634375+MurmUrren@users.noreply.github.com>
2023-05-21 23:36:05 -0700

    Update Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -11,16 +11,16 @@ class Menu {
     }
 
     public void showMenu() {
-        System.out.println("=== Linked List Program ===");
-        System.out.println("1. Insert Node on Head");
-        System.out.println("2. Insert Node on Tail");
-        System.out.println("3. Insert Node on Given Position");
-        System.out.println("4. Delete Head Node");
-        System.out.println("5. Delete Tail Node");
-        System.out.println("6. Delete Node on Given Position");
-        System.out.println("7. Display Linked List");
-        System.out.println("8. Clear Linked List");
-        System.out.println("0. Exit");
+        System.out.println("=== Programa Linked List ===");
+        System.out.println("1. Insertar nodo en la cabecera");
+        System.out.println("2. Insertar nodo en la cola);
+        System.out.println("3. Insertar nodo en x posicion");
+        System.out.println("4. Eliminar nodo de la cabecera");
+        System.out.println("5. Eliminar nodo de la cola");
+        System.out.println("6. Eliminar nodo en x posicion");
+        System.out.println("7. Mostrar Linked List");
+        System.out.println("8. Limpiar Linked List");
+        System.out.println("0. Salir");
         System.out.println("==========================");
     }
 
@@ -29,7 +29,7 @@ class Menu {
 
         while (option != 0) {
             showMenu();
-            System.out.print("Enter your choice: ");
+            System.out.print("Ingresa la opcion deseada: ");
             option = scanner.nextInt();
 
             switch (option) {
@@ -41,8 +41,8 @@ class Menu {
                 case 6 -> deleteNodeOnGivenPosition();
                 case 7 -> displayLinkedList();
                 case 8 -> clearLinkedList();
-                case 0 -> System.out.println("Exiting...");
-                default -> System.out.println("Invalid option. Please try again.");
+                case 0 -> System.out.println("Finalizando programa");
+                default -> System.out.println("Opcion invalida.");
             }
             System.out.println();
         }
@@ -51,61 +51,60 @@ class Menu {
     }
 
     private void insertNodeOnHead() {
-        System.out.print("Enter the data for the new node: ");
+        System.out.print("Ingresa el valor a agregar en el nuevo nodo: ");
         int data;
         try {
             data = scanner.nextInt();
             linkedList.insertHead(data);
             System.out.println("Node inserted at the head.");
         } catch (InputMismatchException e) {
-            System.out.println("Invalid input. Please enter an integer.");
-            scanner.nextLine(); // Clear the input buffer
+            System.out.println("El dato introducido es invalido. Porfavor introduce un entero.");
+            scanner.nextLine();
         }
     }
 
     private void insertNodeOnTail() {
-        System.out.print("Enter the data for the new node: ");
+        System.out.print("Ingresa el valor a agregar en el nuevo nodo: ");
         int data;
         try {
             data = scanner.nextInt();
             linkedList.insertTail(data);
-            System.out.println("Node inserted at the tail.");
+            System.out.println("Nodo insertado en la cola.");
         } catch (InputMismatchException e) {
-            System.out.println("Invalid input. Please enter an integer.");
-            scanner.nextLine(); // Clear the input buffer
+            System.out.println("El dato introducido es invalido. Porfavor introduce un entero.");
+            scanner.nextLine(); 
         }
     }
 
     private void insertNodeOnGivenPosition() {
-        System.out.print("Enter the data for the new node: ");
+        System.out.print("Ingresa el valor a agregar en el nuevo nodo: ");
         int data;
         try {
             data = scanner.nextInt();
-            System.out.print("Enter the position to insert the node: ");
+            System.out.print("Ingresa la posicion en donde se agregara el nodo: ");
             int position = scanner.nextInt();
             linkedList.insertOnGivenPosition(data, position);
-            //System.out.println("Node inserted at position " + position + ".");
         } catch (InputMismatchException e) {
-            System.out.println("Invalid input. Please enter integers for data and position.");
-            scanner.nextLine(); // Clear the input buffer
+            System.out.println("El dato introducido es invalido. Porfavor introduce un entero.");
+            scanner.nextLine(); 
         }
     }
 
     private void deleteHeadNode() {
         linkedList.deleteHead();
-        System.out.println("Head node deleted.");
+        System.out.println("Nodo cabecera eliminado.");
     }
 
     private void deleteTailNode() {
         linkedList.deleteTail();
-        System.out.println("Tail node deleted.");
+        System.out.println("Nodo cola eliminado.");
     }
 
     private void deleteNodeOnGivenPosition() {
-        System.out.print("Enter the position of the node to delete: ");
+        System.out.print("Ingresa la posicion del nodo a eliminar: ");
         int position = scanner.nextInt();
         linkedList.deleteOnGivenPosition(position);
-        System.out.println("Node at position " + position + " deleted.");
+        System.out.println("Nodo en la posicion " + position + " eliminado.");
     }
 
     private void displayLinkedList() {
@@ -115,6 +114,6 @@ class Menu {
 
     private void clearLinkedList() {
         linkedList.clear();
-        System.out.println("Linked List cleared.");
+        System.out.println("Se limpio la Linked List.");
     }
 }
